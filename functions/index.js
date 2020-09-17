485fc89c21aa2d481d0e4760b4e8f548e6bf340d
Khant Nyi Nyi <khantnyinyi4301@gmail.com>
2020-09-18 01:14:03 +0630

    User Registration

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,35 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin"); //admin import
-
+const app = require("express")();
 admin.initializeApp(); //usually pass the project id in parameter but in fitebaserc project default id already given
 
-const express = require("express");
-const app = express();
+const config = {
+  apiKey: "AIzaSyD79FIgh2C4t9ivPulviWk6ICPZwDzgosI",
+  authDomain: "socialape-fff4a.firebaseapp.com",
+  databaseURL: "https://socialape-fff4a.firebaseio.com",
+  projectId: "socialape-fff4a",
+  storageBucket: "socialape-fff4a.appspot.com",
+  messagingSenderId: "960780037836",
+  appId: "1:960780037836:web:0bd0852b8f6d0978f49c69",
+  measurementId: "G-W7N3EX04JG",
+};
+
+const firebase = require("firebase");
+const { json } = require("express");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
-    .orderBy('createdAt', 'desc')
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
     .get() //in order to access to database we have use admin.
     .then((data) => {
       let screams = [];
       data.forEach((doc) => {
         screams.push({
           screamId: doc.id,
-          ...doc.data()
+          ...doc.data(),
         });
       });
       return res.json(screams);
@@ -34,9 +46,7 @@ app.post("/scream", (req, res) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
@@ -47,6 +57,53 @@ app.post("/scream", (req, res) => {
     });
 });
 
-// https://baseurl.com/api/
+//Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
-exports.api = functions.region('asia-southeast2').https.onRequest(app);
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)    //handle as a document ID
+    })
+    .then(() => {
+      return res.status(201).json({token});
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.region("asia-southeast2").https.onRequest(app);
