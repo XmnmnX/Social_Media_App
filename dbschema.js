7b5a9a93727b02217bf6d7c33a113d01bfbc00fc
Khant Nyi Nyi <khantnyinyi4301@gmail.com>
2020-09-17 14:31:11 +0630

    Express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-09-17T07:43:11.261Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,52 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin'); //admin import
+const functions = require("firebase-functions");
+const admin = require("firebase-admin"); //admin import
 
 admin.initializeApp(); //usually pass the project id in parameter but in fitebaserc project default id already given
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require("express");
+const app = express();
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .orderBy('createdAt', 'desc')
+    .get() //in order to access to database we have use admin.
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          ...doc.data()
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
 
- exports.getScreams = functions.https.onRequest((req,res) => {  //this function get data from the database 
-  admin.firestore().collection('screams').get() //in order to access to database we have use admin.
-  .then(data => {
-    let screams = [];
-    data.forEach(doc => {
-      screams.push(doc.data());
-    });
-    return res.json(screams);
-  })
-  .catch(err => console.error(err));
- })
+app.post("/scream", (req, res) => {
+  //this function will post data to the database
 
- exports.createScream = functions.https.onRequest((req,res) => { //this function will post data to the database
-  if(req.method !== 'POST'){
-    return res.status(400).json({error: 'Method not allowed'});
-  }
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString(),
   };
 
   admin
-  .firestore()
-  .collection('screams')
-  .add(newScream)
-  .then(doc => {
-    res.json({message: `document ${doc.id} created successfully`});
-  })
-  .catch(err => {
-    res.status(500).json({error: 'somethings went wrong'});
-    console.error(err);
-  });
- });
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "somethings went wrong" });
+      console.error(err);
+    });
+});
+
+// https://baseurl.com/api/
+
+exports.api = functions.region('asia-southeast2').https.onRequest(app);
