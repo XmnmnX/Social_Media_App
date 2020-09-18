7e27875302b9ab25348149c63e5ee50ea9a68649
Khant Nyi Nyi <khantnyinyi4301@gmail.com>
2020-09-18 19:13:46 +0630

    #5 Validation & Login Route

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,6 @@ const config = {
 };
 
 const firebase = require("firebase");
-const { json } = require("express");
 firebase.initializeApp(config);
 
 const db = admin.firestore();
@@ -56,6 +55,16 @@ app.post("/scream", (req, res) => {
       console.error(err);
     });
 });
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
 
 //Signup route
 app.post("/signup", (req, res) => {
@@ -65,6 +74,20 @@ app.post("/signup", (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+  if (isEmpty(newUser.password)) (errors.password = "Must not be empty");
+  if (newUser.password !== newUser.confirmPassword)
+     (errors.confirmPassword = "Passwords must be the same");
+  if (isEmpty(newUser.handle))  (errors.handle = "Must not be empty");
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   // TODO: validate data
   let token, userId;
@@ -89,12 +112,12 @@ app.post("/signup", (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
-        userId
+        userId,
       };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials)    //handle as a document ID
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials); //handle as a document ID
     })
     .then(() => {
-      return res.status(201).json({token});
+      return res.status(201).json({ token }); //token use whenever we need to access a router that is protected
     })
     .catch((err) => {
       console.error(err);
@@ -107,3 +130,31 @@ app.post("/signup", (req, res) => {
 });
 
 exports.api = functions.region("asia-southeast2").https.onRequest(app);
+
+
+app.post('/login', (req,res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if(isEmpty(user.email)) errors.email = "Must not be empty";
+  if(isEmpty(user.password)) errors.password = "Must not be empty";
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+  .then(data => {
+    return data.user.getIdToken();
+  })
+  .then(token => {
+    return res.json({token});
+  })
+  .catch(err => {
+    console.error(err);
+    if(err.code === 'auth/wrong-password') return res.status(403).json({general: 'Wrong credential, please try again'});
+    else return res.status(500).json({error: err.code});
+  })
+})
